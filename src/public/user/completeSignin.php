35820771bb9377d7c9484cf45f44b7910dac3c8e
satomi212 <matarou0212@gmail.com>
2022-03-14 00:04:23 +0900

    ログイン機能のリファクタリング

--- src/public/user/completeSignin.php
+++ src/public/user/completeSignin.php
@@ -1,52 +1,46 @@
 <?php
+session_start();
+
 // DB接続
 $db['user_name'] = 'root';
 $db['password'] = 'password';
-$pdo = new PDO("mysql:host=mysql; dbname=blog; charset=utf8", $db['user_name'], $db['password']);
+$pdo = new PDO(
+    'mysql:host=mysql; dbname=blog; charset=utf8',
+    $db['user_name'],
+    $db['password']
+);
 
-session_start();
-
-// ログイン状態だったらトップページへ
-if (isset($_SESSION["id"])) {
-    session_regenerate_id(TRUE);
-    header("Location: ../index.php");
-    exit();
-}
 
 $email = filter_input(INPUT_POST, 'email');
 $password = filter_input(INPUT_POST, 'password');
 
-// Email取得
-$sql = "SELECT * FROM users WHERE email = :email";
-$statement = $pdo->prepare($sql);
 
-// 実行
+// Email取得
+$sql = 'SELECT * FROM users WHERE email = :email';
+$statement = $pdo->prepare($sql);
 $statement->bindValue(':email', $email, PDO::PARAM_STR);
 $statement->execute();
 $users = $statement->fetch(PDO::FETCH_ASSOC);
 
+
 // バリデーション
 if (empty($email) || empty($password)) $_SESSION['errors'][] = '「Email」または「パスワード」を入力してください';
 
-if (!password_verify($password, $users['password'])) {
-    $_SESSION['errors'][] = 'パスワードが違います';
-}
+if (!password_verify($password, $users['password'])) $_SESSION['errors'][] = 'パスワードが違います';
 
 
 // エラーなかったら実行
 if (!empty($_SESSION['errors'])) {
-    header("location: ./signin.php");
+    header('location: ./signin.php');
     exit();
 } else {
     $statement->execute();
-    // セッションidの発行
-    session_regenerate_id(TRUE);
-
     // セッションにログイン情報を登録
     $_SESSION['id'] = $users['id'];
     $_SESSION['name'] = $users['name'];
+    $_SESSION['email'] = $users['email'];
 
-    header("location: ../index.php");
+    header('location: ../index.php');
     exit();
 }
 ?>
