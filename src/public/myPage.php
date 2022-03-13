286a35134caea3d3884ee399ce2f52d769c37569
satomi212 <matarou0212@gmail.com>
2022-03-13 23:15:30 +0900

    マイページのヘッダーの追加、リファクタリング

--- src/public/myPage.php
+++ src/public/myPage.php
@@ -3,25 +3,27 @@ session_start();
 
 //ログインされていない場合は強制的にログインページへ
 if (!isset($_SESSION['id'])) {
-    header("Location: ./user/signin.php");
+    header('Location: ./user/signin.php');
     exit();
 }
 
 // DB接続
 $db['user_name'] = 'root';
 $db['password'] = 'password';
-$pdo = new PDO("mysql:host=mysql; dbname=blog; charset=utf8", $db['user_name'], $db['password']);
-
+$pdo = new PDO(
+    'mysql:host=mysql; dbname=blog; charset=utf8',
+    $db['user_name'],
+    $db['password']
+);
 
 // 記事取得
-$sql = "SELECT * FROM blogs WHERE user_id = :id";
+$sql = 'SELECT * FROM blogs WHERE user_id = :id';
 
 // 実行
 $statement = $pdo->prepare($sql);
 $statement->bindValue(':id', $_SESSION['id'], PDO::PARAM_INT);
 $statement->execute();
 $blogs = $statement->fetchAll(PDO::FETCH_ASSOC);
-
 ?>
 
 <!DOCTYPE html>
@@ -30,12 +32,27 @@ $blogs = $statement->fetchAll(PDO::FETCH_ASSOC);
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link href="https://unpkg.com/tailwindcss@^2/dist/tailwind.min.css" rel="stylesheet">
     <title>マイページ</title>
 </head>
 <body>
-
     <header>
-        <?php include('./header.php'); ?>
+        <div class="w-full">
+            <nav class="bg-white shadow-lg">
+                <div class="md:flex items-center justify-between py-2 px-8 md:px-12">
+                    <div class="flex justify-between items-center">
+                        <div class="text-2xl font-bold text-gray-800 md:text-3xl">
+                            Blogアプリ
+                        </div>
+                    </div>
+                    <div class="flex flex-col md:flex-row hidden md:block -mx-2">
+                        <a href="index.php" class="text-gray-800 rounded hover:bg-gray-900 hover:text-gray-100 hover:font-medium py-2 px-2 md:mx-2">ホーム</a>
+                        <a href="myPage.php" class="text-gray-800 rounded hover:bg-gray-900 hover:text-gray-100 hover:font-medium py-2 px-2 md:mx-2">マイページ</a>
+                        <a href="./user/signout.php" class="text-gray-800 rounded hover:bg-gray-900 hover:text-gray-100 hover:font-medium py-2 px-2 md:mx-2">ログアウト</a>
+                    </div>
+                </div>
+            </nav>
+        </div>
     </header>
 
     <main>
@@ -56,14 +73,20 @@ $blogs = $statement->fetchAll(PDO::FETCH_ASSOC);
                     <!-- 15文字以降は...で表示 -->
                     <?php $limit = 15; ?>
                     <?php if (mb_strlen($blog['contents']) > $limit): ?>
-                        <?php $result = mb_substr($blog['contents'], 0, $limit); ?>
+                        <?php $result = mb_substr(
+                            $blog['contents'],
+                            0,
+                            $limit
+                        ); ?>
                         <?php echo $result . '…'; ?>
                     <?php else: ?>
                         <?php echo $blog['contents']; ?>
                     <?php endif; ?>
 
                 <div class="button">
-                    <form action="./myArticleDetail.php?id=<?php echo $blog['id']; ?>" method="post">
+                    <form action="./myArticleDetail.php?id=<?php echo $blog[
+                        'id'
+                    ]; ?>" method="post">
                         <button type="submit">記事詳細へ</button>
                     </form>
                 </div>
