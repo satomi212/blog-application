39aa527cde2424752e383032fc9b339ca8a498c3
satomi212 <matarou0212@gmail.com>
2022-03-15 20:32:36 +0900

    波括弧削除

--- src/public/user/completeSignin.php
+++ src/public/user/completeSignin.php
@@ -2,10 +2,8 @@
 $email = filter_input(INPUT_POST, 'email');
 $password = filter_input(INPUT_POST, 'password');
 
-
 session_start();
 if (empty($email) || empty($password)) $_SESSION['errors'] = 'パスワードとメールアドレスを入力してください';
-
 
 // DB接続
 $db['user_name'] = 'root';
@@ -16,7 +14,6 @@ $pdo = new PDO(
     $db['password']
 );
 
-
 // Email取得
 $sql = 'SELECT * FROM users WHERE email = :email';
 $statement = $pdo->prepare($sql);
@@ -24,13 +21,11 @@ $statement->bindValue(':email', $email, PDO::PARAM_STR);
 $statement->execute();
 $users = $statement->fetch(PDO::FETCH_ASSOC);
 
-
 if (!password_verify($password, $users['password'])) {
     $_SESSION['errors'] = 'メールアドレスまたはパスワードが違います';
     header('location: ./signin.php');
     exit();
 }
-
 
 $_SESSION['id'] = $users['id'];
 $_SESSION['name'] = $users['name'];
