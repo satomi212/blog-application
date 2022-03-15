84890e4a087c0c632b096261860d0a10e77e6199
satomi212 <matarou0212@gmail.com>
2022-03-15 20:21:41 +0900

    test

--- src/public/user/completeSignup.php
+++ src/public/user/completeSignup.php
@@ -1,8 +1,9 @@
 <?php
 // セッションへ格納
 session_start();
-$_SESSION['email'] = filter_input(INPUT_POST, 'email');
+$_SESSION['email'] = filter_input(INPUT_POST, 'email');;
 $_SESSION['name'] = filter_input(INPUT_POST, 'name');
+
 
 // DB接続
 $db['user_name'] = 'root';
@@ -13,24 +14,23 @@ $pdo = new PDO(
     $db['password']
 );
 
+
 // ポスト情報を変数へ
 $name = filter_input(INPUT_POST, 'name');
 $email = filter_input(INPUT_POST, 'email');
 $password = filter_input(INPUT_POST, 'password');
 $confirm = filter_input(INPUT_POST, 'confirm');
 
-if (empty($password) || empty($confirm)) {
-    $_SESSION['errors'][] = 'パスワードを入力してください';
-}
 
-if ($password != $confirm) {
-    $_SESSION['errors'][] = 'パスワードが一致していません';
-}
+if (empty($password) || empty($confirm)) $_SESSION['errors'][] = 'パスワードを入力してください';
+
+if ($password != $confirm) $_SESSION['errors'][] = 'パスワードが一致していません';
 
 if (!empty($_SESSION['errors'])) {
     header('location: ./signup.php');
     exit();
 }
+
 
 // Email取得
 $sql = 'SELECT * FROM users WHERE email = :email';
@@ -39,15 +39,14 @@ $statement->bindValue(':email', $email, PDO::PARAM_STR);
 $statement->execute();
 $users = $statement->fetch();
 
-$available = !$users ? true : false;
-if (!$available) {
-    $_SESSION['errors'][] = '既に登録されているEmailです';
-}
+$available = (!$users) ? true : false;
+if (!$available) $_SESSION['errors'][] = '既に登録されているEmailです';
 
 if (!empty($_SESSION['errors'])) {
     header('location: ./signup.php');
     exit();
 }
+
 
 // 会員情報の登録
 $hashedPassword = password_hash($password, PASSWORD_DEFAULT);
@@ -60,7 +59,7 @@ $statement->bindValue(':email', $email, PDO::PARAM_STR);
 $statement->bindValue(':password', $hashedPassword, PDO::PARAM_STR);
 $statement->execute();
 
-$_SESSION['registed'] = '登録できました';
+$_SESSION['registed'] = "登録できました";
 header('location: ./signin.php');
 exit();
 ?>
